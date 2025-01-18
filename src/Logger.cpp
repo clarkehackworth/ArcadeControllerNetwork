80330daf48f8b6cda46a6aabffbf97362d5f8e50
Jeff Shouse <clarke.hackworth@gmail.com>
2025-01-17 19:51:25 -0600

    logger flush update and formatting update

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -1,76 +1,92 @@
 #include "Logger.h"
 
-Logger::Logger(){
-
+Logger::Logger()
+{
 }
 
-void Logger::setup() {
+void Logger::setup()
+{
   Serial.begin(9600);
-  if (!SD.begin(chipSelect)) {
+  if (!SD.begin(chipSelect))
+  {
     Serial.println("SD Card failed, or not present");
     return;
-  } else {
-    enable = true; 
+  }
+  else
+  {
+    enable = true;
     dataFile = SD.open(filename, FILE_WRITE);
     log("SD Card Initialized");
   }
-
-  
 }
 
-void Logger::log(String data) {
-  
-  if(enable && LOG_TO_SD){
-    //Serial.print("Writing ");
-    //Serial.println(data);
-    
+void Logger::log(String data)
+{
+
+  if (enable && LOG_TO_SD)
+  {
+    // Serial.print("Writing ");
+    // Serial.println(data);
+
     // dataFile = SD.open(filename, FILE_WRITE);
-    dataFile.println("[+] "+data);
+    dataFile.println("[+] " + data);
     dataFile.flush();
     // dataFile.close();
-    //Serial.println("log written");
+    // Serial.println("log written");
   }
-  if(LOG_TO_SERIAL){
-    Serial.println("[+] "+data);
+  if (LOG_TO_SERIAL)
+  {
+    Serial.println("[+] " + data);
   }
 }
 
-void Logger::error(String data) {
-  
-  if(enable && LOG_TO_SD){
-    //Serial.print("Writing ");
-    //Serial.println(data);
-    
+void Logger::error(String data)
+{
+
+  if (enable && LOG_TO_SD)
+  {
+    // Serial.print("Writing ");
+    // Serial.println(data);
+
     // dataFile = SD.open(filename, FILE_WRITE);
-    dataFile.println("[#] "+data);
+    dataFile.println("[#] " + data);
     dataFile.flush();
     // dataFile.close();
-    //Serial.println("log written");
+    // Serial.println("log written");
   }
-  if(LOG_TO_SERIAL){
-    Serial.println("[#] "+data);
+  if (LOG_TO_SERIAL)
+  {
+    Serial.println("[#] " + data);
   }
 }
 
-void Logger::debug(String data) {
-  if(DEBUG){
-    if(enable && LOG_TO_SD){
-      //Serial.print("Writing ");
-      //Serial.println(data);
-      // dataFile = SD.open(filename, FILE_WRITE);
-      dataFile.println("[d] "+data);
+void Logger::debug(String data)
+{
+  if (DEBUG)
+  {
+    if (enable && LOG_TO_SD)
+    {
+      // Serial.print("Writing ");
+      // Serial.println(data);
+      //  dataFile = SD.open(filename, FILE_WRITE);
+      dataFile.println("[d] " + data);
       dataFile.flush();
       // dataFile.close();
-      //Serial.println("log written");
+      // Serial.println("log written");
     }
-    if(LOG_TO_SERIAL){
-      Serial.println("[d] "+data);
+    if (LOG_TO_SERIAL)
+    {
+      Serial.println("[d] " + data);
     }
   }
 }
 
-void Logger::flush() {
-  if(enable && LOG_TO_SD){
+void Logger::flush()
+{
+  if (enable && LOG_TO_SD)
+  {
     dataFile.flush();
   }
+  if (enable)
+    Serial.flush();
 }
